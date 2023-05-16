4d57e505cdf9909779a55018e2af422e537edcf6
Colin Frick <colin+github@sonderformat.llc>
2023-05-16 16:33:12 +0200

    fix: use correct client Id

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FusionAuthModule } from '@fusionauth/angular-sdk';
   imports: [
     BrowserModule,
     FusionAuthModule.forRoot({
-      clientId: 'e9fdb985-9173-4e01-9d73-ac2d60d1dc8f',
+      clientId: 'e9fdb985-9173-4e01-9d73-ac2d60d1dc8e',
       serverUrl: 'http://localhost:9011',
       redirectUri: 'http://localhost:4200'
     })
